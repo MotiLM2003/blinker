62ae26e4da9a64f1d4cbe1c08f816819f05eba5f
Moti Elmakies <motiphone2003@gmail.com>
2021-01-14 13:18:21 +0200

    dasdasd

--- js.js
+++ js.js
@@ -6,8 +6,40 @@ const getInputs = () => {
   inputs[lastIndex - 1];
 };
 
-let list = document.querySelectorAll('.child');
+console.log($(window).width());
 
+const screen = $(window).width();
+let maxChar = 0;
+
+if (screen >= 1200) {
+  maxChar = 78;
+} else if (screen >= 1000) {
+  maxChar = 66;
+} else if (screen >= 800) {
+  maxChar = 53;
+} else if (screen >= 600) {
+  maxChar = 35;
+} else if (screen >= 400) {
+  maxChar = 23;
+}
+
+const dade = document.getElementById('fade');
+const thanks = document.getElementById('thanks');
+const btnContaienr = document.getElementById('button-container');
+const icon = document.getElementById('v-icon');
+let list = document.querySelectorAll('.child');
+$(icon).fadeOut(0);
+document.getElementById('button-container').addEventListener('click', () => {
+  //   fade.classList.remove('hide');
+  btnContaienr.style.background = 'transparent';
+  $(btnContaienr).addClass('blink');
+  setTimeout(() => {
+    $(thanks).fadeOut(1000);
+    // $('#v-icon').fadeIn();
+    $(icon).fadeIn(1000);
+    $(btnContaienr).removeClass('blink');
+  }, 5000);
+});
 getInputs();
 
 $(inputs[currentIndex]).focus();
@@ -19,6 +51,7 @@ jQuery.extend(jQuery.expr[':'], {
 });
 
 $(document).on('keyup', 'input,select', function (e) {
+  console.log($(this).width(), $(this).val().length);
   if (e.which == 8 && $(this).val() === '') {
     console.log($(this).parent().prev().html());
     const prevInput = $(this).parent().prev().first();
@@ -28,7 +61,7 @@ $(document).on('keyup', 'input,select', function (e) {
     currentIndex -= 1;
     all.eq(currentIndex).focus();
   }
-  if (e.which == 13) {
+  if (e.which == 13 || $(this).val().length >= maxChar) {
     currentIndex += 1;
     if (this === inputs[lastIndex - 1]) {
       $('.contaienr').append(
