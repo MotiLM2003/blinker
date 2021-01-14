4d5c7aa33bbbb300cf2cc3d109f02bb0d6e6bc48
Moti Elmakies <motiphone2003@gmail.com>
2021-01-14 10:45:48 +0200

    dasds

--- js.js
+++ js.js
@@ -1,5 +1,16 @@
+let inputs = document.getElementsByTagName('input');
+let lastIndex = 0;
+let currentIndex = 0;
+const getInputs = () => {
+  lastIndex = inputs.length;
+  inputs[lastIndex - 1];
+};
+
 let list = document.querySelectorAll('.child');
 
+getInputs();
+
+$(inputs[currentIndex]).focus();
 // register jQuery extension
 jQuery.extend(jQuery.expr[':'], {
   focusable: function (el, index, selector) {
@@ -7,8 +18,24 @@ jQuery.extend(jQuery.expr[':'], {
   },
 });
 
-$(document).on('keypress', 'input,select', function (e) {
+$(document).on('keyup', 'input,select', function (e) {
+  if (e.which == 8 && $(this).val() === '') {
+    console.log($(this).parent().prev().html());
+    const prevInput = $(this).parent().prev().first();
+    console.log(prevInput.html());
+    var all = $(':focusable');
+    if (currentIndex <= 0) return;
+    currentIndex -= 1;
+    all.eq(currentIndex).focus();
+  }
   if (e.which == 13) {
+    currentIndex += 1;
+    if (this === inputs[lastIndex - 1]) {
+      $('.contaienr').append(
+        '<div><input type="text" class="input" value=""  /></div>'
+      );
+      getInputs();
+    }
     e.preventDefault();
     // Get all focusable elements on the page
     var $canfocus = $(':focusable');
@@ -16,28 +43,25 @@ $(document).on('keypress', 'input,select', function (e) {
     if (index >= $canfocus.length) index = 0;
     $canfocus.eq(index).focus();
   }
+
   var me = $(this);
   if (isEnglish(me.val())) {
     me.css('text-align', 'left');
   } else {
-    console.log(me.val());
     me.css('text-align', 'right');
   }
-  console.log($(this).val());
 });
 
 const isEnglish = (text) => {
   const paragraph = text;
-  const reg = '^[a-zA-Z0-9]*$';
+  const reg = '^[a-zA-Z0-9 ,;./]*$';
   const found = paragraph.match(reg);
-  console.log(found);
+
   return found;
 };
 
 Array.from(list).forEach(function (e) {
-  e.addEventListener('click', () => {
-    console.log('click');
-  });
+  e.addEventListener('click', () => {});
 });
 
 Array.from(list).forEach(function (e) {
