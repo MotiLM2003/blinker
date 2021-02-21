02fb40f062ea0ea9db9458d612c987414cd8da16
Moti Elmakies <motiphone2003@gmail.com>
2021-02-21 16:50:54 +0200

    asdsa

--- js.js
+++ js.js
@@ -1,55 +1,44 @@
-let inputs = document.getElementsByTagName('input');
-let lastIndex = 0;
-let currentIndex = 0;
-const getInputs = () => {
-  lastIndex = inputs.length;
-  inputs[lastIndex - 1];
-};
+let quill = null;
+$(document).ready(function () {
+  quill = new Quill('#editor', { placeholder: '' });
+  quill.focus();
+  // $('.ql-editor p').text('a').focus();
 
-const screen = $(window).width();
-let maxChar = 0;
-
-console.log(screen);
-
-// return maxChar > screen;
-if (screen >= 2400) {
-  maxChar = 1800;
-} else if (screen >= 2000) {
-  maxChar = 120;
-} else if (screen >= 1700) {
-  maxChar = 80;
-} else if (screen >= 1200) {
-  maxChar = 78;
-} else if (screen >= 1000) {
-  maxChar = 66;
-} else if (screen >= 800) {
-  maxChar = 53;
-} else if (screen >= 600) {
-  maxChar = 35;
-} else if (screen >= 400) {
-  maxChar = 22;
-} else if (screen >= 300) {
-  maxChar = 20;
-} else if (screen >= 270) {
-  maxChar = 10;
-}
+  $('#bookmarkme').click(function () {
+    if (window.sidebar && window.sidebar.addPanel) {
+      // Mozilla Firefox Bookmark
+      window.sidebar.addPanel(document.title, window.location.href, '');
+    } else if (window.external && 'AddFavorite' in window.external) {
+      // IE Favorite
+      window.external.AddFavorite(location.href, document.title);
+    } else if (window.opera && window.print) {
+      // Opera Hotlist
+      this.title = document.title;
+      return true;
+    } else {
+      // webkit - safari/chrome
+      alert(
+        'Press ' +
+          (navigator.userAgent.toLowerCase().indexOf('mac') != -1
+            ? 'Command/Cmd'
+            : 'CTRL') +
+          ' + D to bookmark this page.'
+      );
+    }
+  });
+});
 
 const clickEvent = () => {
   let hasLetters = false;
-  $('input[type="text"]').each(function (index) {
-    let me = $(this);
 
-    const value = me.val();
-    if (value !== '') {
-      hasLetters = true;
-    }
-  });
+  hasLetters = quill.getText(0, 10).trim() !== '';
 
   if (!hasLetters) {
     $('#arrow').addClass('blink2');
-    $('#first').focus();
+    quill.focus();
     setTimeout(() => {
       $('#arrow').removeClass('blink2');
+      quill.focus();
     }, 2500);
     return;
   }
@@ -62,14 +51,20 @@ const clickEvent = () => {
 
   setTimeout(() => {
     $(thanks).fadeOut(1000);
-    $('input[type="text"]')
-      .css({ color: 'white' })
-      .attr('disabled', 'disabled');
+    $('.ql-editor p').fadeOut(500);
+    quill.disable();
     $(icon).fadeIn(1000);
     $(btnContaienr).removeClass('blink');
   }, 5000);
 };
 
+const isEnglish = (text) => {
+  const paragraph = text;
+  const reg = '^[a-zA-Z0-9 ,;./!,]*$';
+  const found = paragraph.match(reg);
+
+  return found;
+};
 const dade = document.getElementById('fade');
 const thanks = document.getElementById('thanks');
 const btnContaienr = document.getElementById('button-container');
@@ -80,9 +75,8 @@ document
   .getElementById('button-container')
   .addEventListener('click', clickEvent);
 
-getInputs();
+// getInputs();
 
-$(inputs[currentIndex]).focus();
 // register jQuery extension
 jQuery.extend(jQuery.expr[':'], {
   focusable: function (el, index, selector) {
@@ -90,59 +84,49 @@ jQuery.extend(jQuery.expr[':'], {
   },
 });
 
-$(document).on('keydown', 'input,select', function (e) {
-  if (e.which == 8 && $(this).val() === '') {
-    console.log('in delete');
-    const prevInput = $(this).parent().prev().first().find('input');
-    prevInput.focus();
-    console.log(prevInput.html());
-    // var all = $(':focusable');
-    // if (currentIndex <= 0) return;
-    // currentIndex -= 1;
-    // al
-    l.eq(currentIndex).focus();
-  }
-  if (e.which == 13 || $(this).val().length >= maxChar) {
-    currentIndex += 1;
-    if (this === inputs[lastIndex - 1]) {
-      $('.contaienr').append(
-        '<div><input type="text" class="input" value=""  /></div>'
-      );
-      getInputs();
-    }
+function contains_heb(str) {
+  console.log(str);
+  return /[\u0590-\u05FF]/.test(str);
+}
 
-    e.preventDefault();
-    // Get all focusable elements on the page
-    var $canfocus = $(':focusable');
-    var index = $canfocus.index(this) + 1;
-    if (index >= $canfocus.length) index = 0;
-    $canfocus.eq(index).focus();
-  }
-
-  var me = $(this);
-  if (isEnglish(me.val())) {
-    me.css('text-align', 'left');
-  } else {
-    me.css('text-align', 'right');
-  }
-});
-
-const isEnglish = (text) => {
-  const paragraph = text;
-  const reg = '^[a-zA-Z0-9 ,;./!,]*$';
-  const found = paragraph.match(reg);
-
-  return found;
-};
-
-Array.from(list).forEach(function (e) {
-  e.addEventListener('click', () => {});
-});
-
-Array.from(list).forEach(function (e) {
-  e.addEventListener('keyup', () => {
-    if (e.code == 'Enter') {
-      console.log('pressed enter');
+$(document).on('keydown', function (e) {
+  $('.ql-editor p').each(function () {
+    var me = $(this);
+    const isHeb = contains_heb(me.text());
+    me.removeClass('rtl');
+    if (isHeb) {
+      me.addClass('rtl');
     }
   });
+  // if (e.which == 8 && $(this).val() === '') {
+  //   console.log('in delete');
+  //   const prevInput = $(this).parent().prev().first().find('input');
+  //   prevInput.focus();
+  //   console.log(prevInput.html());
+  //   // var all = $(':focusable');
+  //   // if (currentIndex <= 0) return;
+  //   // currentIndex -= 1;
+  //   // al
+  //   l.eq(currentIndex).focus();
+  // }
+  // if (e.which == 13 || $(this).val().length >= maxChar) {
+  //   currentIndex += 1;
+  //   if (this === inputs[lastIndex - 1]) {
+  //     $('.contaienr').append(
+  //       '<div><input type="text" class="input" value=""  /></div>'
+  //     );
+  //   }
+  //   e.preventDefault();
+  //   // Get all focusable elements on the page
+  //   var $canfocus = $(':focusable');
+  //   var index = $canfocus.index(this) + 1;
+  //   if (index >= $canfocus.length) index = 0;
+  //   $canfocus.eq(index).focus();
+  // }
+  // var me = $(this);
+  // if (isEnglish(me.val())) {
+  //   me.css('text-align', 'left');
+  // } else {
+  //   me.css('text-align', 'right');
+  // }
 });
